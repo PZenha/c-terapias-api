c982646a88c6dc596e27afe1838638fef69bedc5
Pedro Zenha <pedrozenha12@gmail.com>
2021-03-06 02:27:07 +0000

    middlewares

--- src/graphql/typeDefs/index.ts
+++ src/graphql/typeDefs/index.ts
@@ -1,8 +1,16 @@
+import { applyMiddleware } from 'graphql-middleware'
+import { makeExecutableSchema } from 'apollo-server'
 import client from './client'
 import observation from './observation'
 import auth from './auth'
 import appointment from './appointment'
+import permissions from '../permissions'
+import resolvers from '../resolvers'
 
 const typeDefs = [client, observation, auth, appointment]
+
+// const schema = applyMiddleware(makeExecutableSchema({ typeDefs, resolvers }), permissions)
+
+// const schema = makeExecutableSchema({ typeDefs, resolvers: (resolvers as any) })
 
 export default typeDefs
